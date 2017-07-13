c49cf769740de8a8bebb0207f64a77083e1369ec
andremakram <andre.makram@gmail.com>
2017-07-12 23:11:13 -0700

    Create andre.js

---
+++ andre.js
@@ -0,0 +1,11 @@
+/*
+@title Request Query String
+@query
+{
+  "example" : "$top=5&$skip=2&$select=name,age,species"
+}
+*/
+
+module.exports = function(req, res, next) {
+  res.send(req.query);
+};
